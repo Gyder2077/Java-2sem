e8b0343869e148cb52735c287117fdfcffc12b2b
Анатолий Мухортов <amuhortov10@gmail.com>
2026-03-03 19:59:37 +0300

    lab5-8

--- src/main/java/main/Collection/MyCollection.java
+++ src/main/java/main/Collection/MyCollection.java
@@ -5,10 +5,11 @@ import main.Given.Ticket;
 import java.util.*;
 
 public class MyCollection {
-    private ArrayDeque<Ticket> myCollection = new ArrayDeque<>();
+    private static ArrayDeque<Ticket> myCollection;
+    private static long nextId = 0;
     private static final MyCollection Instance = new MyCollection();
 
-    private MyCollection() {}
+    private MyCollection() {myCollection = new ArrayDeque<>();}
 
     public static MyCollection getInstance() {
         return Instance;
@@ -18,7 +19,41 @@ public class MyCollection {
         return myCollection;
     }
 
-    public void setMyCollection(ArrayDeque<Ticket> myCollection) {
-        this.myCollection = myCollection;
+    public static void setMyCollection(ArrayDeque<Ticket> myCollection) {
+        MyCollection.myCollection = myCollection;
     }
+
+    public static long getNextId() {
+        nextId += 1;
+        return nextId;
+    }
+
+    public void addElement(Ticket ticket) {
+        myCollection.add(ticket);
+        Ticket[] array = myCollection.toArray(new Ticket[0]);
+        Arrays.sort(array);
+        myCollection.clear();
+        Collections.addAll(myCollection, array);
+    }
+
+    public boolean delElement(long id) {
+        Iterator<Ticket> iterator = myCollection.iterator();
+        while (iterator.hasNext()) {
+            Ticket t = iterator.next();
+            if (t.getId() == id) {
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+     public String showElement(long id) {
+         for (Ticket t : myCollection) {
+             if (t.getId() == id) {
+                 return t.toString();
+             }
+         }
+         throw new IllegalArgumentException("No element with such ID");
+     }
 }
